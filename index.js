b15c0e638c5c1eabee021fd604bd0ecd3c55f92a
Vijay Killu <mvkillu@gmail.com>
2025-03-18 21:22:44 +0530

    Mail with attachments

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ const mg = mailgun.client({
   key: process.env.MAILGUN_API_KEY
 });
 
-const data = {
+// Send mail without attachment
+let data = {
   from: 'Excited User <mailgun@sandbox.mailgun.net>',
-  to: 'mvkillu@gmail.com', // This email must be authorized in your Mailgun account
+  to: 'mvkillu@gmail.com', 
   subject: 'Hello',
   text: 'Testing some Mailgun awesomeness!'
 };
@@ -19,5 +20,18 @@ const data = {
 mg.messages.create(process.env.MAILGUN_DOMAIN, data)
   .then(msg => console.log(msg))
   .catch(err => console.log(err));
+
+  
+  // Send mail with attachment  
+ data = {
+    from: 'Excited User <mailgun@sandbox.mailgun.net>',
+    to: 'mvkillu@gmail.com',
+    subject: 'Hello',
+    text: 'Testing some Mailgun awesomeness!',
+    attachment: {
+      filename: 'test.txt',
+      content: 'Hello World'
+    }
+  };
 
   
